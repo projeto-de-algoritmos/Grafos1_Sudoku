df4f6f7180b9069b502e6fd7a901169d5005a856
LimirioGuimaraes <limirioguimaraes@gmail.com>
2023-04-23 16:49:13 -0300

    atualização: as posções dos elementos são armazenadas agora em um grafo, organizado como se fosse uma matriz 9x9

--- sudoku.py
+++ sudoku.py
@@ -1,8 +1,20 @@
+import networkx as nx
 import pygame
 from pygame.locals import *
 from sys import exit
 
 pygame.init()
+
+# Criando grafo que representa uma matriz 9x9 para gerar as posições dos elementos numéricos do sudoku
+grafo_tabuleiro = nx.Graph()
+# Adicionando os nós dentro do grafo
+for i in range(9):
+    for j in range(9):
+        grafo_tabuleiro.add_node((i, j))
+# Iniciando cada elemento do grafo com 0
+for i in range(9):
+    for j in range(9):
+        grafo_tabuleiro.nodes[i, j][repr(int)] = 0
 
 tela = pygame.display.set_mode((725, 725))
 pygame.display.set_caption('Sudoku')
@@ -13,19 +25,14 @@ cinza_claro = (220, 220, 220)
 cinza_escuro = (170, 170, 170)
 vermelho = (255, 0, 0)
 preto = (0, 0, 0)
-
+# define tamanho e qual fonte iremos utilizar em algumas strings
 fonte = pygame.font.SysFont('Arial', 40)
 # define o fundo da tela como branco
 tela.fill((255, 255, 255))
-
-# Definição da dimensão do tabuleiro, necessária para a confecção da interface gráfica
-dimensao = 9
 # Definição do tamanho em píxeis de cada quadrado do tabuleiro, no caso 50x50
 tamanho_quadrado = 75
 # Espaçamento entre os retângulos do tabuleiro de sudoku
 espacamento = 5
-# matriz de elementos que representam os valores dentro do tabuleiro iniciando as 91 posições com 0
-tabuleiro = {(i, j): 0 for i in range(dimensao) for j in range(dimensao)}
 # iniciando uma célula como selecionado, para não quebrar a função
 i_sel, j_sel = 0, 0
 
@@ -43,17 +50,17 @@ def desenhar_tabuleiro():
     pygame.draw.line(tela, preto, (0, 723), (725, 723), 6)  # borda inferior
     pygame.draw.line(tela, preto, (723, 0), (723, 723), 5)  # borda direita
 
-    for i in range(dimensao):
-        for j in range(dimensao):
+    for i in range(9):
+        for j in range(9):
             # loopings para adicionar quadrados de cores diferentes, formando o tabuleiro
             x = espacamento + j * (tamanho_quadrado + espacamento)
             y = espacamento + i * (tamanho_quadrado + espacamento)
             cor = cinza_escuro if (i + j) % 2 == 0 else cinza_claro
             pygame.draw.rect(tela, cor, (x, y, tamanho_quadrado, tamanho_quadrado))
             # renderizar na tela o número de cada elemento da matriz
-            if tabuleiro[(i, j)] != 0:  # verifica se a posição (i, j), do tabuleiro difere de 0
+            if grafo_tabuleiro.nodes[(i, j)][repr(int)] != 0:  # verifica se a posição (i, j), do tabuleiro difere de 0
                 # gera uma string com o número que o player digitou
-                numero = fonte.render(str(tabuleiro[(i, j)]), True, preto)
+                numero = fonte.render(str(grafo_tabuleiro.nodes[(i, j)][repr(int)]), True, preto)
                 # a função caixa_de_numero cria um quadrado que pode renderizar texto, é já define o número a ser
                 # exibido no centro do retângulo selecionado
                 caixa_de_numero = numero.get_rect(center=(x + (tamanho_quadrado / 2), y + (tamanho_quadrado / 2)))
@@ -70,7 +77,7 @@ while True:
         elif event.type == KEYDOWN:  # verifica se alguma tecla foi pressionada
             if event.unicode.isdigit():
                 # atualiza o valor dentro da célula da matriz de elementos do tabuleiro, conforme o valor digitado
-                tabuleiro[(i_sel, j_sel)] = int(event.unicode)
+                grafo_tabuleiro.nodes[(i_sel, j_sel)][repr(int)] = int(event.unicode)
         # verifica em qual quadrado o player clicou
         elif event.type == MOUSEBUTTONDOWN:
             # atualiza as coordenadas da célula selecionada
