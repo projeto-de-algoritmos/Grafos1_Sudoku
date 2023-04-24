6fc65e8a59687f4e84af1dc64aff953a8453bde3
LimirioGuimaraes <limirioguimaraes@gmail.com>
2023-04-24 18:59:19 -0300

    melhorias na organização do codigo

--- telas_de_verificacao.py
+++ telas_de_verificacao.py
@@ -4,48 +4,48 @@ import tkinter
 
 def tela_vitoria():
     # Criando a janela
-    root = tkinter.Tk()
-    root.title("Parabéns")
+    pop_up = tkinter.Tk()
+    pop_up.title("Parabéns")
     mensagem = "Parabéns você ganhou! :)"
     # Cria um label com a mensagem
-    label = tkinter.Label(root, text=mensagem, pady=10)
+    label = tkinter.Label(pop_up, text=mensagem, pady=10)
     label.pack()
     # Cria um botão para fechar a janela
-    button = tkinter.Button(root, text="OK", command=root.destroy)
+    button = tkinter.Button(pop_up, text="OK", command=pop_up.destroy)
     button.pack()
-    root.geometry("300x100")
+    pop_up.geometry("300x100")
     # Define as coordenadas para o centro da tela
-    root.update_idletasks()
-    largura = root.winfo_width()
-    altura = root.winfo_height()
-    x = (root.winfo_screenwidth() // 2) - (largura // 2)
-    y = (root.winfo_screenheight() // 2) - (altura // 2)
+    pop_up.update_idletasks()
+    largura = pop_up.winfo_width()
+    altura = pop_up.winfo_height()
+    x = (pop_up.winfo_screenwidth() // 2) - (largura // 2)
+    y = (pop_up.winfo_screenheight() // 2) - (altura // 2)
     # Definindo posição do pop-up na tela
-    root.geometry('{}x{}+{}+{}'.format(largura, altura, x, y))
+    pop_up.geometry('{}x{}+{}+{}'.format(largura, altura, x, y))
     # Espera o usuário apertar no ok, após isso encerra o jogo
-    root.mainloop()
+    pop_up.mainloop()
     pygame.quit()
     exit()
 
 
 def tela_resposta_errada():
     # Criando a janela
-    root = tkinter.Tk()
-    root.title("Você Consegue!")
+    pop_up = tkinter.Tk()
+    pop_up.title("Você Consegue!")
     mensagem = "Resposta Incorreta :(\nTente mais um pouco!"
     # Cria um label com a mensagem
-    label = tkinter.Label(root, text=mensagem, pady=10)
+    label = tkinter.Label(pop_up, text=mensagem, pady=10)
     label.pack()
     # Cria um botão para fechar a janela
-    button = tkinter.Button(root, text="OK", command=root.destroy)
+    button = tkinter.Button(pop_up, text="OK", command=pop_up.destroy)
     button.pack()
-    root.geometry("300x100")
+    pop_up.geometry("300x100")
     # Define as coordenadas para o centro da tela
-    root.update_idletasks()
-    largura = root.winfo_width()
-    altura = root.winfo_height()
-    x = (root.winfo_screenwidth() // 2) - (largura // 2)
-    y = (root.winfo_screenheight() // 2) - (altura // 2)
+    pop_up.update_idletasks()
+    largura = pop_up.winfo_width()
+    altura = pop_up.winfo_height()
+    x = (pop_up.winfo_screenwidth() // 2) - (largura // 2)
+    y = (pop_up.winfo_screenheight() // 2) - (altura // 2)
     # Definindo posição do pop-up na tela
-    root.geometry('{}x{}+{}+{}'.format(largura, altura, x, y))
-    root.mainloop()
+    pop_up.geometry('{}x{}+{}+{}'.format(largura, altura, x, y))
+    pop_up.mainloop()
