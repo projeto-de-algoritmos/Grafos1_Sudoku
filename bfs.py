5bf671938b57a960145424f93d0312f8c76089f4
LimirioGuimaraes <limirioguimaraes@gmail.com>
2023-04-30 16:11:09 -0300

    Atualização da funçao que faz a verificação do tabuleiro

--- bfs.py
+++ bfs.py
@@ -1,55 +1,42 @@
+
 def verificar_entrada(grafo_tabuleiro):
+    # Cria uma fila para armazenar os nós a serem visitados
     fila = []
-        if grafo_tabuleiro.nodes[(linha, coluna)]['valor'] == 0:
-            for x in range(1, 10):
-                if numero_valido(grafo_tabuleiro, linha, coluna, x):
-                    grafo_tabuleiro.nodes[(linha, coluna)]['valor'] = x
-                    fila.append((linha, coluna))
-                    break
-            else:
-                # Nenhum valor é válido, volta para o nó anterior
-                grafo_tabuleiro.nodes[(linha, coluna)]['valor'] = 0
-                if fila:
-                    linha, coluna = fila[-1]
-                else:
-                    # Todos os nós foram preenchidos e verificados
-                    break
-        else:
-            if not numero_valido(grafo_tabuleiro, linha, coluna, grafo_tabuleiro.nodes[(linha, coluna)]['valor']):
+    # Cria um conjunto para armazenar os nós já visitados
+    visitados = set()
+    # Adiciona o primeiro nó na fila
+    fila.append((0, 0))
+    # Enquanto a fila não estiver vazia, repete o seguinte:
+    while fila:
+        # Remove o primeiro nó da fila e o marca como visitado
+        i, j = fila.pop(0)
+        visitados.add((i, j))
+        # Obtém o valor do nó atual
+        valor = grafo_tabuleiro.nodes[(i, j)]['valor']
+        # Verifica se o valor é distinto dos valores dos vizinhos na mesma linha, coluna ou bloco 3x3
+        for k, l in pega_vizinhos(i, j):
+            if grafo_tabuleiro.nodes[(k, l)]['valor'] == valor:
                 return False
-            for i, j in pega_vizinhos(linha, coluna):
-                if grafo_tabuleiro.nodes[(i, j)]['valor'] == 0:
-                    fila.append((i, j))
-
+            # Se o vizinho ainda não foi visitado, adiciona ele na fila
+            if (k, l) not in visitados:
+                fila.append((k, l))
+    # Se todas as verificações passaram, o sudoku é válido
     return True
 
 
-def numero_valido(grafo_tabuleiro, linha, coluna, x):
-    if x in [grafo_tabuleiro.nodes[(linha, j)]['valor'] for j in range(9)]:
-        return False
-    if x in [grafo_tabuleiro.nodes[(i, coluna)]['valor'] for i in range(9)]:
-        return False
-    linha_bloco, coluna_bloco = linha // 3, coluna // 3
-    for i in range(linha_bloco * 3, linha_bloco * 3 + 3):
-        for j in range(coluna_bloco * 3, coluna_bloco * 3 + 3):
-            if grafo_tabuleiro.nodes[(i, j)]['valor'] == x:
-                return False
-    return True
-
-
-def pega_vizinhos(linha, coluna):
+def pega_vizinhos(i, j):
+    # Retorna uma lista com os índices dos vizinhos de um nó na mesma linha, coluna ou bloco 3x3
     vizinhos = []
-    for i in range(9):
-        if i != coluna:
-            vizinhos.append((linha, i))
-        if i != linha:
-            vizinhos.append((i, coluna))
+    for k in range(9):
+        if k != j:
+            vizinhos.append((i, k))
+        if k != i:
+            vizinhos.append((k, j))
 
-    sublinha = linha // 3
-    subcoluna = coluna // 3
-    for i in range(sublinha * 3, sublinha * 3 + 3):
-        for j in range(subcoluna * 3, subcoluna * 3 + 3):
-            if i != linha or j != coluna:
-                vizinhos.append((i, j))
-
+    sublinha = i // 3
+    subcoluna = j // 3
+    for k in range(sublinha * 3, sublinha * 3 + 3):
+        for l in range(subcoluna * 3, subcoluna * 3 + 3):
+            if k != i or l != j:
+                vizinhos.append((k, l))
     return vizinhos
--- tabuleiros.py
+++ tabuleiros.py
@@ -1,65 +1,5 @@
 def tabuleiro_fixo(grafo_tabuleiro):
-    # Criando board fixo para não ser necessário gerar um aleatório a toda nova interação
 
-    grafo_tabuleiro.nodes[(0, 1)]['valor'] = 3
-    grafo_tabuleiro.nodes[(0, 2)]['valor'] = 4
-    grafo_tabuleiro.nodes[(0, 3)]['valor'] = 6
-    grafo_tabuleiro.nodes[(0, 4)]['valor'] = 7
-    grafo_tabuleiro.nodes[(0, 6)]['valor'] = 8
-    grafo_tabuleiro.nodes[(0, 7)]['valor'] = 9
-    grafo_tabuleiro.nodes[(1, 1)]['valor'] = 9
-    grafo_tabuleiro.nodes[(1, 3)]['valor'] = 5
-    grafo_tabuleiro.nodes[(1, 4)]['valor'] = 1
-    grafo_tabuleiro.nodes[(1, 5)]['valor'] = 8
-    grafo_tabuleiro.nodes[(1, 6)]['valor'] = 3
-    grafo_tabuleiro.nodes[(1, 7)]['valor'] = 7
-    grafo_tabuleiro.nodes[(1, 8)]['valor'] = 4
-    grafo_tabuleiro.nodes[(2, 0)]['valor'] = 8
-    grafo_tabuleiro.nodes[(2, 2)]['valor'] = 3
-    grafo_tabuleiro.nodes[(2, 6)]['valor'] = 2
-    grafo_tabuleiro.nodes[(2, 7)]['valor'] = 5
-    grafo_tabuleiro.nodes[(2, 8)]['valor'] = 6
-    grafo_tabuleiro.nodes[(3, 0)]['valor'] = 4
-    grafo_tabuleiro.nodes[(3, 1)]['valor'] = 1
-    grafo_tabuleiro.nodes[(3, 2)]['valor'] = 5
-    grafo_tabuleiro.nodes[(3, 4)]['valor'] = 8
-    grafo_tabuleiro.nodes[(3, 5)]['valor'] = 6
-    grafo_tabuleiro.nodes[(3, 7)]['valor'] = 3
-    grafo_tabuleiro.nodes[(4, 2)]['valor'] = 9
-    grafo_tabuleiro.nodes[(4, 4)]['valor'] = 2
-    grafo_tabuleiro.nodes[(4, 6)]['valor'] = 7
-    grafo_tabuleiro.nodes[(4, 8)]['valor'] = 5
-    grafo_tabuleiro.nodes[(5, 0)]['valor'] = 7
-    grafo_tabuleiro.nodes[(5, 1)]['valor'] = 8
-    grafo_tabuleiro.nodes[(5, 3)]['valor'] = 9
-    grafo_tabuleiro.nodes[(5, 4)]['valor'] = 5
-    grafo_tabuleiro.nodes[(5, 5)]['valor'] = 1
-    grafo_tabuleiro.nodes[(5, 6)]['valor'] = 4
-    grafo_tabuleiro.nodes[(5, 8)]['valor'] = 2
-    grafo_tabuleiro.nodes[(6, 0)]['valor'] = 9
-    grafo_tabuleiro.nodes[(6, 1)]['valor'] = 4
-    grafo_tabuleiro.nodes[(6, 3)]['valor'] = 2
-    grafo_tabuleiro.nodes[(6, 3)]['valor'] = 2
-    grafo_tabuleiro.nodes[(6, 4)]['valor'] = 5
-    grafo_tabuleiro.nodes[(6, 5)]['valor'] = 7
-    grafo_tabuleiro.nodes[(6, 7)]['valor'] = 8
-    grafo_tabuleiro.nodes[(6, 8)]['valor'] = 1
-    grafo_tabuleiro.nodes[(7, 0)]['valor'] = 3
-    grafo_tabuleiro.nodes[(7, 1)]['valor'] = 2
-    grafo_tabuleiro.nodes[(7, 2)]['valor'] = 5
-    grafo_tabuleiro.nodes[(7, 4)]['valor'] = 1
-    grafo_tabuleiro.nodes[(7, 6)]['valor'] = 6
-    grafo_tabuleiro.nodes[(7, 7)]['valor'] = 7
-    grafo_tabuleiro.nodes[(7, 8)]['valor'] = 9
-    grafo_tabuleiro.nodes[(8, 1)]['valor'] = 8
-    grafo_tabuleiro.nodes[(8, 2)]['valor'] = 7
-    grafo_tabuleiro.nodes[(8, 3)]['valor'] = 6
-    grafo_tabuleiro.nodes[(8, 4)]['valor'] = 9
-    grafo_tabuleiro.nodes[(8, 5)]['valor'] = 2
-    grafo_tabuleiro.nodes[(8, 7)]['valor'] = 5
-    grafo_tabuleiro.nodes[(8, 8)]['valor'] = 3
-
-    ## apagar daqui pra cima
     grafo_tabuleiro.nodes[(0, 5)]['valor'] = 7
     grafo_tabuleiro.nodes[(0, 8)]['valor'] = 1
     grafo_tabuleiro.nodes[(1, 0)]['valor'] = 6
@@ -84,7 +24,6 @@ def tabuleiro_fixo(grafo_tabuleiro):
     grafo_tabuleiro.nodes[(7, 5)]['valor'] = 6
     grafo_tabuleiro.nodes[(8, 0)]['valor'] = 5
     grafo_tabuleiro.nodes[(8, 6)]['valor'] = 9
-
 
 
 def verificar_coordenadas(i, j):
